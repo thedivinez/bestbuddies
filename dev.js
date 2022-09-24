0d5537d809476aeda9df3a82468ec3825d33ea2a
Divine Zvenyika <dzbusiness01@gmail.com>
2022-09-24 20:22:40 +0200

    change images

--- dev.js
+++ dev.js
@@ -1,12 +1,18 @@
 import path from 'path';
 import express from 'express';
+import livereload from "livereload";
 import services from "./services.js";
 import { engine } from 'express-handlebars';
+import connectLiveReload from "connect-livereload";
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => setTimeout(() => liveReloadServer.refresh("/"), 100));
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(connectLiveReload());
 app.set('view engine', '.hbs');
 app.use(express.static("public"));
 
@@ -15,6 +21,8 @@ app.engine('.hbs', engine({
   layoutsDir: path.join("views", "layouts"),
   partialsDir: path.join("views", "partials"),
 }));
+
+
 
 const getService = (target) => services.find((service) => service.id === target)
 
